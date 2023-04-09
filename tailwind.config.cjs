7a24b6450e398cf73f00ab469d439c5bba215784
giovani.franz <giovani.franz@agi.com.br>
2023-04-09 08:09:47 -0300

    :rocket:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,9 @@ module.exports = {
         title: ['Open Sans', 'sans-serif'],
       },
       backgroundImage: {
-        texture: "url('./src/img/bg.webp')",
-        bulbs: "url('./src/img/bulbs.webp')",
-        calculator: "url('./src/img/calculator.webp')",
+        texture: "url('/bg.webp')",
+        bulbs: "url('/bulbs.webp')",
+        calculator: "url('/calculator.webp')",
       },
     },
   },
